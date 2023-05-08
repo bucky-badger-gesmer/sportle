8aa2f5e9f49cabaceed97340758b265d54fb9541
bucky-badger-gesmer <ajgesmer@comcast.net>
2023-05-08 17:53:08 -0500

    app calls playerIndex query

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from "@apollo/client";
+
+const GET_PLAYER_INDEX = gql`
+  query PlayerIndex {
+    nba {
+      playerIndex {
+        id
+        lastName
+        firstName
+        playerSlug
+        team {
+          id
+          slug
+          city
+          name
+          abbreviation
+        }
+        jerseyNumber
+        position
+        height
+        weight
+        college
+        country
+        draft {
+          year
+          round
+          pick
+        }
+        active
+        headlineStats {
+          points
+          rebounds
+          assists
+          timeFrame
+        }
+        career {
+          fromYear
+          toYear
+        }
+      }
+    }
+  }
+`;
+
 
 function App() {
+  const { loading, error, data } = useQuery(GET_PLAYER_INDEX);
+
+  console.log('poop', data)
+
+  if (loading) {
+    return (<>loading...</>)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>There have been {data.nba.playerIndex.length} players to play in the NBA!</h1>
     </div>
   );
 }
