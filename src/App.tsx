7d686ca723f278015148e91c64acde3e5dd4af91
bucky-badger-gesmer <ajgesmer@comcast.net>
2023-05-10 14:26:31 -0500

    change to guess game!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import { useQuery, gql } from "@apollo/client";
 import { PlayerIndexQuery } from "./graphql/globalTypes";
-import { Spin, Table } from "antd";
+import { AutoComplete, Avatar, Input, Spin } from "antd";
+import { useState } from "react";
 
 const GET_PLAYER_INDEX = gql`
   query PlayerIndex {
@@ -46,6 +47,8 @@ const GET_PLAYER_INDEX = gql`
 `;
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [guess, setGuess] = useState();
   const { loading, data, error } = useQuery<PlayerIndexQuery>(GET_PLAYER_INDEX);
 
   if (error) throw error;
@@ -54,55 +57,73 @@ function App() {
 
   if (loading) {
     return (
-      <>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          margin: "auto",
+          width: "100px",
+          height: "100px",
+        }}
+      >
         <Spin size="large" />
-      </>
+      </div>
     );
   }
 
   const players = playerIndex?.length
     ? playerIndex?.map((player) => {
         return {
-          id: player?.id,
-          firstName: player?.firstName,
-          lastName: player?.lastName,
-          position: player?.position,
-          jerseyNumber: player?.jerseyNumber,
+          label: (
+            <>
+              <Avatar
+                size="large"
+                src={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${player?.id}.png`}
+              />{" "}
+              {player?.firstName} {player?.lastName}
+            </>
+          ),
+          value: `${player?.firstName} ${player?.lastName}`,
+          foo: { ...player },
         };
       })
     : [];
 
-  const columns = [
-    {
-      title: "Player ID",
-      dataIndex: "id",
-      key: "id",
-    },
-    {
-      title: "First Name",
-      dataIndex: "firstName",
-      key: "firstName",
-    },
-    {
-      title: "Last Name",
-      dataIndex: "lastName",
-      key: "lastName",
-    },
-    {
-      title: "Position",
-      dataIndex: "position",
-      key: "position",
-    },
-    {
-      title: "Jersey Number",
-      dataIndex: "jerseyNumber",
-      key: "jerseyNumber",
-    },
-  ];
-
+  const randomPlayerIndex = Math.floor(
+    Math.random() * (players.length - 1) + 1
+  );
+  console.log("randomPlayerIndex", randomPlayerIndex);
+  console.log("randomPlayer", players[randomPlayerIndex]);
   return (
     <div className="App">
-      <Table dataSource={players} columns={columns} />
+      {/* <Title level={2}>
+        Guess a player out of the {players.length} players to ever play in the
+        NBA!
+      </Title> */}
+      <AutoComplete
+        options={players.filter((player) =>
+          player.value.toLowerCase().includes(searchTerm.toLowerCase())
+        )}
+        style={{ marginTop: 36 }}
+        onSelect={(player) => setGuess(player)}
+        onChange={(text) => {
+          setSearchTerm(text);
+        }}
+      >
+        <Input.Search
+          allowClear
+          size="large"
+          placeholder="Guess a random NBA player"
+          enterButton="Guess!"
+          onSearch={() => {
+            console.log("pressing guess...", guess);
+          }}
+          disabled={true}
+        />
+      </AutoComplete>
     </div>
   );
 }
